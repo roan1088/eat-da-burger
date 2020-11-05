f4a5458eb844fcf076c5bf71c759ad4627d97ec3
roan1088 <rohan_188@yahoo.com>
2020-11-05 17:27:02 -0500

    Added config to connect to the db

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+// Importing the db connection
+const connection = require("./connection");
+
+// Object Relational Mapper (ORM)
+const orm = {
+    
+};
+
+// Exporting the orm
+module.exports = orm;
