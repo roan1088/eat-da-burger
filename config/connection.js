490ae2e1a084fa0ec310cc87d3656cdc1909415c
roan1088 <rohan_188@yahoo.com>
2020-11-06 10:11:10 -0500

    Added connection config for production on heroku

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,23 @@
 const mysql = require("mysql");
 
 // Setting up connection config
-const connection = mysql.createConnection({
-    host: "localhost",
-    // Your port; if not 3306
-    port: 3306,
-    // Your username
-    user: "root",
-    // Your password
-    password: "mysql",
-    database: "burgers_db"
-});
+let connection;
+// Check if we have Jaws DB URL
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        // Your port; if not 3306
+        port: 3306,
+        // Your username
+        user: "root",
+        // Your password
+        password: "mysql",
+        database: "burgers_db"
+    });
+}
 
 // Connecting to the db
 connection.connect(function(err) {
