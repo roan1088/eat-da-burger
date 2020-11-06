39af6e306d4dd7290372cd450bca49fc2b8f66c9
roan1088 <rohan_188@yahoo.com>
2020-11-05 19:15:02 -0500

    Added routes to the server

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Setting the Express App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +17,10 @@ app.use(express.json());
 // Setting up handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 // Starting the server
 app.listen(PORT, function() {
